92bf8788cc587f1714a592f7ed928d27e3745c2b
laurajauch <laurajauch@users.noreply.github.com>
2015-04-02 15:46:49 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,6 +5,7 @@ kivy.require('1.0.7')
 from kivy.app import App
 
 class testApp(App):
+    #Design elements are containted in the test.kv file which is kivy looks for when the program starts.
     pass
              
 
