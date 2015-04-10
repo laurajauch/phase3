5e6c94f5206bfd9dd18aeb100276d035dff16405
ninacamillone <nina.camillone@my.wheaton.edu>
2015-04-10 13:11:57 -0500

    starting code for storing data

---
+++ InputData.py
@@ -0,0 +1,183 @@
+from ConditionParser import *
+from ParseFunction import *
+from SolveFormulation import *
+import re
+
+# The memento doesn't care about any of the data, it just passes it around
+class Memento:
+    def __init__(self, dataMap):
+        self.set(dataMap)
+    def get(self):
+        return self.dataMap
+    def set(self, dataMap):
+        self.dataMap = dataMap
+
+class InputData:
+    def __init__(self, stokesOrNot):
+        self.vars = {"stokes": stokesOrNot} # to collect all the variables
+
+        # Stokes: stokesTrue, transient, dims [], numElements[], mesh, 
+        #   polyOrder, inflow tuple (numInflows, [inflow regions], [x velocities], [y velocities]),
+        #   outflow tuple (numOutflows, [outflow regions]), wall tuple (numWalls, [wall regions])
+        # Navier Stokes: nStokesFalse, Reynolds, transient, dims[], numElements[], mesh, polyOrder, 
+        #   inflow tuple, outflow tuple, wall tuple
+
+    def setForm(self, form):
+        self.vars["form"] = form
+    def getForm(self):
+        try:
+            return self.vars["form"]
+        except:
+            print("InputData does not contain form")
+    def addVariable(self, string, var):
+        self.vars[string] = var
+    def getVariable(self, string):
+        try: 
+            return self.vars[string]
+        except:
+            pass
+    def createMemento(self):
+        return Memento(self.vars)
+    def setMemento(self, memento):
+        self.vars = memento.get()
+
+	def storeReynolds(self, datum):
+	    try:
+            self.addVariable("reynolds",int(datum))
+            return True
+	    except ValueError:
+	        return False
+
+
+	def storeState(self, datum):
+            try:
+                datumL = datum.lower().strip()
+                if datumL == "transient" or datumL == "steady state":
+                    if datumL == "steady state":
+                        self.addVariable("transient", False) # steady state
+                        return True
+                    elif (not self.getVariable("stokes")) and datumL == "transient":
+                        print("Transient solves are not supported for Navier-Stokes")
+                        return False
+                    else:
+                        self.addVariable("transient", True)
+                        return True
+                else:
+                    return False
+            except AttributeError:
+                return False
+	    
+	def storeMeshDims(self, datum):
+		try:
+		    dims = stringToDims(str(datum).strip())
+		    self.addVariable("meshDimensions", dims)
+		    return True
+		except ValueError:
+		    return False
+		
+	def storeNumElements(self, inputData, datum):
+		try:
+		    numElements = stringToElements(str(datum).strip())
+                    self.addVariable("numElements", numElements)
+		    return True
+		except ValueError:
+		    return False
+	    
+	def storePolyOrder(self, inputData, datum):
+		try:
+			order = int(datum)
+			if order <= 9 and order >= 1:
+			    self.addVariable("polyOrder",order)
+			    return True
+			else:
+			    return False
+		except ValueError:
+			return False
+	    
+	def storenumInflows(self, datum):
+	    try:
+	        self.addVariable("numInflows", int(datum))
+	    except ValueError:
+	        return False
+	        
+	def storeInflows(self, rawRegions, rawYs, rawXs)
+	    Regions = []
+	    Ys = []
+	    Xs = []
+	    i = 1
+	    while i <= self.numInflows:
+	        try:
+	            Regions.append(stringToFilter(rawRegions[i]))
+	        except ValueError:
+	            return False
+	        try:
+	            Ys.append(stringToFilter(rawYs[i]))
+	        except ValueError:
+	            return False
+	        try:
+	            Xs.append(stringToFilter(rawXs[i]))
+	        except ValueError:
+	            return False
+	    self.addVariable("inflowRegions", Regions)
+	    self.addVariable("inflowX", Xs)
+	    self.addVariable("inflowY", Ys)
+	    return True
+	    
+	    
+def storenumOutflows(self, datum): 
+	    try:
+	        self.addVariable("numOutflows", int(datum))
+	    except ValueError:
+	        return False
+	        
+	def storeOutflows(self, rawRegions)
+	    Regions = []
+	    Ys = []
+	    Xs = []
+	    i = 1
+	    while i <= self.numInflows:
+	        try:
+	            Regions.append(stringToFilter(rawRegions[i]))
+	        except ValueError:
+	            return False
+	    self.addVariable("outflowRegions", Regions)
+	    return True
+	    
+	def storenumWalls(self, datum):
+	    try:
+	         inputData.addVariable("numWalls", int(datum))
+	    except ValueError:
+	        return False
+	        
+	def storeWalls(self, datum):
+	    Regions = []
+	    i = 1
+	    while i <= self.numWalls:
+	        try:
+	            Regions.append(stringToFilter(rawRegions[i]))
+	        except ValueError:
+	            return False
+	    self.addVariable("wallRegions", Regions)
+	    return True
+	    
+
+"""
+Some methods for formatting data input
+"""
+def stringToDims(inputstr):
+    try:
+        tokenList = re.split('x', inputstr)
+        x = float(tokenList[0])
+        y = float(tokenList[1])
+        return [x,y]
+    except:
+        raise ValueError
+
+def stringToElements(inputstr):
+    try:
+        tokenList = re.split('x', inputstr)
+        x = int(tokenList[0])
+        y = int(tokenList[1])
+        return [x,y]
+    except:
+        raise ValueError
