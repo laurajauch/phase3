160300b910f7d725dbec3e935aa0a74cd150a1f3
Kelly Breedlove <kelly.breedlove@my.wheaton.edu>
2015-04-14 22:27:52 -0500

    added load save to model

--- Model.py
+++ Model.py
@@ -4,22 +4,25 @@ import InputData
 """
 Model
 
-
+The model in the MVC pattern. This class holds the state of 
+the PyCamellia solver at all times.
 """
 class Model(object):
 
     def __init___(self):
         self.form = None
-           
+        
+    """
+    Precondition: data contains the following - Navier/Stokes, Transient/Steady, Renolds 
+    (Navier only), mesh dimensions, initial number of elements, polynomial order, 
+    inflow regions list, inflow x velocity list, inflow y velocity list, outflow regions list
+    Param: data The data...
 
-    #precondition
-    #data contains the following: Navier/Stokes, Transient/Steady, Renolds (Navier only), mesh dimensions, initial number of elements, polynomial order, inflow regions list, inflow x velocity list, inflow y velocity list, outflow regions list
-    #coming soon: determining wall regions
+    Coming Soon: determining wall regions
+    """
     def enterData(self, data):
         errorMsg = {} #True for stored data, false for errors
         
-            
-
     def solve(self):
         FormUtils.solve(self.form)
 
@@ -27,19 +30,62 @@ class Model(object):
         #Plotter.plot()
         pass
         
-    def refine(self:
+    def refine(self):
         pass
 
-    
-
-
-
-
-
-
-
-
-
+    """
+    Save to the specified file
+    Param: filename The name of the file to save to
+    """
+    def save(self, filename):
+        form = context.inputData.getForm()
+        form.save(filename)
+        
+        memento = context.inputData.createMemento()
+        output = memento.get()
+        if len(output) >= 9:
+            del output["form"]
+            del output["inflowRegions"]
+            del output["inflowX"]
+            del output["inflowY"]
+            del output["outflowRegions"]
+            del output["wallRegions"]
+            
+            saveFile = open(filename, 'wb')
+            pickle.dump(memento, saveFile)
+            saveFile.close()
+    """
+    Load from the specified file
+    Param: filename The name fo the file to load from
+    """
+    def load(self, filename):
+        try:           
+            loadFile = open(filename)
+            memento = pickle.load(loadFile)
+            loadFile.close()
+            context.inputData = InputData(True)
+            context.inputData.setMemento(memento)
+            
+            polyOrder = context.inputData.getVariable("polyOrder")
+            spaceDim = 2
+            if not context.inputData.getVariable("stokes"):
+                reynolds = context.inputData.getVariable("reynolds")
+                form = NavierStokesVGPForumlation(filename, spaceDim, reynolds, polyOrder)
+            else:
+                useConformingTraces = False
+                mu = 1.0
+                form = StokesVGPFormulation(spaceDim, useConformingTraces, mu)
+                form.initializeSolution(filename, polyOrder)
+                
+                context.inputData.setForm(form)
+                
+                mesh = form.solution().mesh()
+                elementCount = mesh.numActiveElements()
+                globalDofCount = mesh.numGlobalDofs()
+                
+        except (OSError, IOError):
+            print("No solution was found with the name \"%s\"" % command)
+            
 
     if __name)) == '__main__':
         pass
