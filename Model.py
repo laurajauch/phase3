ba64631da77059ad6b5ab39192f2b12281f1b4f4
Kelly Breedlove <kelly.breedlove@my.wheaton.edu>
2015-04-14 20:23:50 -0500

    added files for Model and View Translator

---
+++ Model.py
@@ -0,0 +1,26 @@
+
+"""
+Model.py
+
+The mediator between the view (test.kv) and the controller components.
+
+"""
+
+class Model():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name)) == '__main__':
+    pass
---
+++ ViewTranslator.py
@@ -0,0 +1,14 @@
+"""
+View Translator
+
+A small class with the specific purpose to read and write to 
+the view (test.kv, run with Main.py)
+
+"""
+
+class ViewTranslator():
+
+
+
+    if __name)) == '__main__':
+        pass
