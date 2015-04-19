e6c532298a770c3039552e41ffd90f2d3b8023f2
PAUL HAEFLIGER <paul.haefliger@my.wheaton.edu>
2015-04-18 19:08:14 -0500

    Adding what I hadn't pushed

--- Controller.py
+++ Controller.py
@@ -1,8 +1,15 @@
 import kivy
 kivy.require('1.0.7')
 from kivy.app import App
-from Model import *
-
+#from Model import *
+import StringIO
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib.figure import Figure
+import numpy as np
+from kivy.core.image.img_pygame import ImageLoaderPygame
+from kivy.properties import ObjectProperty
+from kivy.uix.image import Image
+from kivy.lang import Builder
 """
 Controller
 
@@ -11,9 +18,35 @@ responsibility of reading and writing to the view (test.kv).
 """
 class Controller(object):
     def run(self):
-        self.model = Model()
-        testApp().run()
+        #self.model = Model()
+        testApp(self).run()
+        """This method is called when one of the plot choices is pressed"""
+    def plot(self, plotType):
+        print(plotType)
+        #self.fig = self.model.plot(plotType)
+        #self.image = fig2png(self.fig)
+        #Set the kivy image to self.image
+        
+
+
+        """Convert a matplotlib.Figure to PNG image.:returns: PNG image bytes"""
+    def fig2png(fig):
+  
+        data = StringIO.StringIO()
+        canvas = FigureCanvasAgg(fig)
+        canvas.print_png(data)
+        return data.getvalue()
+
+
     
+    
+    """This method is called when one of the refine choices is pressed"""
+    def refine(self,refineType):
+        print(refineType)
+
+
+
+
 
 """
 Design elements are contained in the test.kv file
@@ -21,11 +54,23 @@ which kivy will look in when the program starts as
 a result of this empty class.
 """
 class testApp(App):
-    pass
+    
+    def refine(self, input):
+        self.controller.refine(input)
+    def plot(self, input):
+        self.controller.plot(input)
+    """
+    Added this build function so we can maipulate testApp when it is created. 
+    We just need to specify which .kv file we are building from.
+    """
+    def build(self):
+        self.controller = Controller()
+        return Builder.load_file('test.kv')
+        
+
 
 
 
 if __name__ == '__main__':
-    controller = Controller()
-    controller.run()
+    testApp().run()
 
