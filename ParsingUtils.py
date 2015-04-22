c0fc615c0f81312c24186fa0cd7e54da4e1c688e
Kelly Breedlove <kelly.breedlove@my.wheaton.edu>
2015-04-21 22:18:30 -0500

    added method for checking input validitiy

--- ParsingUtils.py
+++ ParsingUtils.py
@@ -1,4 +1,5 @@
 import re
+import os.path
 
 """
 Some methods for formatting data input
@@ -21,5 +22,67 @@ def stringToElements(inputstr):
     except:
         raise ValueError
 
+"""
+Check to see if the given data is valid.
+data: A dictionary containing the data to be tested
+return: errors A dictionary mapping string description to boolean, 
+False if field was valid, True if field was invalid
+"""
 def checkInputValidity(data):
-    pass
+    errors = {}
+     
+
+    # reynolds: must be a int and nStokes
+    try:
+        assert not (data["stokes"] and "reynolds" in data) # missmatch
+        assert not data["stokes"] # nStokes
+        assert int(data["reynolds"])
+        errors["reynolds"] = False
+    except:
+        errors["reynolds"] = True
+
+    # polyOrder: must be an int less than 10
+    try:
+        assert int(data["polyOrder"]) < 10
+        assert int(data["polyOrder"]) > 0
+        errors["polyOrder"] = False
+    except:
+        errors["polyOrder"] = True
+        
+    # numElements: int x int
+    try:
+        stringToElements(str(data["numElements"]))
+        errors["numElements"] = False
+    except:
+        errors["numElements"] = True
+
+    # meshDimensions: double x double
+    try:
+        stringToDims(str(data["meshDimensions"]))
+        errors["meshDimensions"] = False
+    except:
+        errors["meshDimensions"] = True
+            
+    # inflowConditions: 
+    try:
+        stringToFilter(str(data["inflow"]))
+        errors["inflow"] = False
+    except:
+        errors["inflow"] = True
+                
+    # outflowConditions:
+    try:
+        stringToFilter(str(data["outflow"]))
+        errors["outflow"] = False
+    except:
+        errors["outflow"] = True
+                    
+    # loadFile: 
+    try:
+        os.path.isfile(str(data["loadFile"])) 
+        errors["loadFile"] = False
+    except:
+        errors["loadFile"] = True
+
+
+    return errors
