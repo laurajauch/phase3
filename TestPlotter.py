c8b257f2af1ca9e68ad4225fadfed89de4bd73d0
Kelly Breedlove <kelly.breedlove@my.wheaton.edu>
2015-04-21 20:13:37 -0500

    added a stub for TestPlotter

---
+++ TestPlotter.py
@@ -0,0 +1,12 @@
+from PyCamellia import *
+from Plotter import *
+import unittest
+
+class TestPlotter(unittest.TestCase):
+
+    """Test Stuff"""
+    def test_stuff(self):
+        pass
+
+    if __name__ == '__main__':
+        unittest.main()
