f822dd1d95dc7484c31ec0aab46976bda5169a5a
Blake Edward Hensel <blake.hensel@cslab06.wheaton.edu>
2015-04-23 22:26:30 -0500

    finished parser

--- runTests.py
+++ runTests.py
@@ -1,10 +1,12 @@
 #from TestFormUtils import * 
 #from TestInputData import *
 from TestDataUtils import *
+from TestParseFunction import *
 #from TestModel import *
 import unittest
 
 testSuite = unittest.makeSuite(TestDataUtils)
+testSuite.addTest(unittest.makeSuite(TestParseFunction))
 #testSuite.addTest(unittest.makeSuite(TestFormUtils))
 #testSuite.addTest(unittest.makeSuite(TestInputData))
 #testSuite.addTest(unittest.makeSuite(TestModel))
