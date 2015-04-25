723a838e9ceee2f0407397087394a1e2cad2b2b2
Kelly Breedlove <kelly.breedlove@my.wheaton.edu>
2015-04-25 14:38:56 -0500

    added capturing so the output isn't loud but commented it out for debug

--- runTests.py
+++ runTests.py
@@ -1,30 +1,31 @@
 from TestConditionParser import *
-#from TestDataUtils import *
-#from TestFormUtils import * 
-#from TestInputData import *
-#from TestModel import *
-#from TestParseFunction import *
+from TestDataUtils import *
+from TestFormUtils import * 
+from TestInputData import *
+from TestModel import *
+from TestParseFunction import *
 from TestParsingUtils import *
 #from TestPlotError import *
 #from TestPlotterP import *
 #from TestPlotter import *
 #from TestPlotterStream import *
+from CapturingUtil import *
 import unittest
 
 testSuite = unittest.makeSuite(TestConditionParser)
-#testSuite.addTest(unittest.makeSuite(TestDataUtils))
-#testSuite.addTest(unittest.makeSuite(TestFormUtils))
-#testSuite.addTest(unittest.makeSuite(TestInputData))
-#testSuite.addTest(unittest.makeSuite(TestModel))
-#testSuite.addTest(unittest.makeSuite(TestParseFunction))
+testSuite.addTest(unittest.makeSuite(TestDataUtils))
+testSuite.addTest(unittest.makeSuite(TestFormUtils)) # runs for a long time
+testSuite.addTest(unittest.makeSuite(TestInputData))
+testSuite.addTest(unittest.makeSuite(TestModel))
+testSuite.addTest(unittest.makeSuite(TestParseFunction))
 testSuite.addTest(unittest.makeSuite(TestParsingUtils))
+
 #testSuite.addTest(unittest.makeSuite(TestPlotError))
 #testSuite.addTest(unittest.makeSuite(TestPlotterP))
 #testSuite.addTest(unittest.makeSuite(TestPlotter))
 #testSuite.addTest(unittest.makeSuite(TestPlotterStream))
 
-
-
 testRunner = unittest.TextTestRunner()
 
+#with Capturing() as output:
 testRunner.run(testSuite)
