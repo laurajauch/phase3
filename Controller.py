372a75ec19e4dd91d43264a059e6ed60f20b3af1
Connor de la Cruz <connor.delacruz@my.wheaton.edu>
2015-04-25 16:53:28 -0500

    Preventing it from crashing when the filename is blank in save too

--- Controller.py
+++ Controller.py
@@ -324,6 +324,8 @@ class ViewApp(App):
         self.controller.pressLoad(filename)
     def save(self):
         filename = self.getFilename()
+        if(filename == ''):
+            return
         self.controller.pressSave(filename)
 
     """
