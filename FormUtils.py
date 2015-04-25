87a86f8be86f9f514bde9eb6a7b67e5831fafaa6
Jeffrey D. Burge <jeffrey.burge@cslab06.wheaton.edu>
2015-04-25 16:11:59 -0500

    editing refine

--- FormUtils.py
+++ FormUtils.py
@@ -153,12 +153,9 @@ def steadyNonlinearInit(spaceDim, re, dims, numElements, polyOrder):
 # Refine----------------------------------------------------------------------
 def autoRefine(data,refType): # refType: 0 is h, 1 is p      why pass data not form?
     
-    """ ret = formInit(data)
-    form = ret[0] 
-    print str(type(form))
-    fType = ret[1]
-    print "executing refine"""
-    """if refType == H:
+    form = data.getVariable("form")
+
+    if refType == H:
         if fType == STEADYLINEAR or fType == TRANSIENTLINEAR:
             form = linearHAutoRefine(form)
         elif fType == STEADYNONLINEAR:
@@ -167,10 +164,10 @@ def autoRefine(data,refType): # refType: 0 is h, 1 is p      why pass data not f
         if fType == STEADYLINEAR or fType == TRANSIENTLINEAR:
             form = linearPAutoRefine(form)
         elif fType == STEADYNONLINEAR:
-            form = nonlinearPAutoRefine(form)"""
+            form = nonlinearPAutoRefine(form)
 
-    #return form 
-    return data["form"]
+    return form 
+    #return data.getVariable("form")
 
 def linearHAutoRefine(form):
     form.hRefine()
